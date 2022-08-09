52ed2dd2c5b8bf5022c01aacdc42e95701fc28ef
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 19:14:02 +0530

    added

---
+++ leapyearchk/leapyearchk/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Leapyearchk;
+public class Leapyearchk
+{
+    public static void Main(string[] args)
+    {
+        int year;
+        Console.Write("Input an year : ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 400) == 0)
+        {
+            Console.WriteLine(year+" is a leap year.\n" );
+        }
+        else if ((year % 100) == 0)
+        {
+
+            Console.WriteLine(year + " is not a leap year.\n");
+        }
+        else if ((year % 4) == 0)
+        {
+            Console.WriteLine(year + "{is a leap year.\n");
+        }
+        else
+            Console.WriteLine(year + " is not a leap year.\n");
+    }
+}
