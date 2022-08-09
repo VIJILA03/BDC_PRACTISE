b8fc761f9b5dd4ffa8afd7201e37ee5c334af160
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 18:34:28 +0530

    added

--- assignments/assignments/reverse3letters.cs
+++ assignments/assignments/reverse3letters.cs
@@ -1,12 +1,12 @@
 ﻿public class reverse3letters
 {
-    public static void Main(string[] args)
+    public static void Main(String[] args)
     {
         char firstletter,secondletter, thirdletter;
         Console.WriteLine("ENTER THREE LETTERS");
         firstletter = Convert.ToChar(Console.ReadLine());
         secondletter = Convert.ToChar(Console.ReadLine());
         thirdletter = Convert.ToChar(Console.ReadLine());
-        Console.WriteLine("REVERSED LETTERS: "+thirdletter+" "+secondletter+" "+firstletter);
+        Console.WriteLine("REVERSED LETTERS:{0} {1} {2} "+thirdletter+" "+secondletter+" "+firstletter);
     }
 }
