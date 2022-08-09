ad8ff61ce6815d570086d7f81cf9310e749016d8
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 19:46:22 +0530

    added

---
+++ triangle/triangle/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Triangle
+{
+    public static void Main()
+    {
+        int side1, side2, side3;
+        Console.WriteLine("Enter side 1:");
+        side1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter side 2:");
+        side2 =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter side 3: ");
+        side3 = Convert.ToInt32(Console.ReadLine());
+        if(side1==side2 && side2==side3)
+        {
+            Console.WriteLine("EQUILATERAL TRIANGLE");
+        }
+        else if(side1==side2 || side2==side3 || side1==side3)
+        {
+            Console.WriteLine("ISOSCELES TRIANGLE");
+        }
+        else
+        {
+            Console.WriteLine("scalene triangle");
+        }
+    }
+}
