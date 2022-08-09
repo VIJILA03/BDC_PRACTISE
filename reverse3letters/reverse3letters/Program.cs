a8c04e5d91980f36ddc38a4520102874930894dd
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 18:39:00 +0530

    added

---
+++ reverse3letters/reverse3letters/Program.cs
@@ -0,0 +1,13 @@
+﻿public class reverse3letters
+{
+    public static void Main(string[] args)
+    {
+        char firstletter, secondletter, thirdletter;
+        Console.WriteLine("ENTER THREE LETTERS");
+        firstletter = Convert.ToChar(Console.ReadLine());
+        secondletter = Convert.ToChar(Console.ReadLine());
+        thirdletter = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("REVERSED LETTERS: " + thirdletter + " " + secondletter + " " + firstletter);
+    }
+}
+
