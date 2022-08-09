9c420febbdffe7b6c1d9eb348138198582c6651f
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 19:22:00 +0530

    added

---
+++ heightrange/heightrange/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace heightrange
+{
+    public class Heightrange
+    {
+        public static void Main(string[] args)
+        {
+            float Heightincm;
+            Console.Write("Enter the height of the person (in centimetres): ");
+            Heightincm = Convert.ToInt32(Console.ReadLine());
+
+            if (Heightincm < 135.0)
+                Console.Write(" SHORT");
+            else if (Heightincm >= 135.0 && Heightincm <= 180.0)
+                Console.Write(" AVERAGE ");
+
+            else
+                Console.Write("TALL");
+        }
+    }
+}
