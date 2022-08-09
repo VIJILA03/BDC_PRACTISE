0c6cc862231929b8f2be48c1e4a7e9d2963831cf
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 19:36:44 +0530

    added

---
+++ admissioncheck/admissioncheck/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Admissioncheck
+{
+    public static void Main(string[] args)
+    {
+        float physics, maths, chemistry;
+        Console.WriteLine("ELIGIBILTY CHECK");
+        Console.WriteLine("Enter Maths mark: ");
+        maths=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Chemsitry mark: ");
+        chemistry =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Physics mark: ");
+        physics =Convert.ToInt32(Console.ReadLine());
+        float total = maths + physics + chemistry;
+        float mathphy = maths + physics;
+        float matchem = maths + chemistry;
+        if(maths>=65 && physics>=55 && chemistry>=50 && (total >= 180 || mathphy >= 140 || matchem >= 140))
+        {
+            Console.WriteLine("YOU ARE ELIGIBLE");
+
+        }
+        else
+        {
+            Console.WriteLine("YOU ARE NOT ELIGIBLE");
+        }
+
+    }
+}
