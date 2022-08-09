664fb74ae19b6644b1e78e7f84e3258c002f7622
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-09 18:54:14 +0530

    added

---
+++ speedinkm/speedinkm/Program.cs
@@ -0,0 +1,33 @@
+﻿public class speedinkm
+{
+    public static void Main(string[] args)
+    {
+        float distance;
+        float hour, min, sec;
+        float timeinsec;
+        float kmperhr, milesperhr;
+
+        Console.Write("Input distance(metres): ");
+        distance = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Input time:\n");
+
+        Console.Write("Input hour value: ");
+        hour = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input minutes value: ");
+        min = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Input seconds value: ");
+        sec = Convert.ToInt32(Console.ReadLine());
+
+        timeinsec = (hour * 3600) + (min * 60) + sec;
+
+        kmperhr = (distance / 1000.0f) / (timeinsec / 3600.0f);
+
+        milesperhr = kmperhr / 1.609f;
+
+        Console.WriteLine("Your speed in km/h is: "+kmperhr);
+        Console.WriteLine("Your speed in miles/h is: "+milesperhr);
+    }
+}
