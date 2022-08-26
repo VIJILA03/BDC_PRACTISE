c66f6731079d534102ff67ccfadf3e857df0f46c
VIJILA D <70993417+VIJILA03@users.noreply.github.com>
2022-08-26 09:32:51 +0530

    ADDED

---
+++ 25aug/25augAssign/25augAssign/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Assign;
+public class aassign
+{
+    public static void Main(String[] args)
+    {
+
+        string connect = ConfigurationManager.ConnectionStrings["MyKey"].ConnectionString;
+        //string connect = "data source=LAPTOP-PH4JPVTP;initial catalog=demo;trusted_connection=true";
+
+        SqlConnection sqlconn = new SqlConnection(connect);
+        Console.WriteLine("connecting to the server");
+        sqlconn.Open();
+        Console.WriteLine("connected to the server");
+
+
+        SqlCommand cmd = new SqlCommand("SELECT * FROM GetHigherBalance()", sqlconn);
+
+        Console.WriteLine("found successfully");
+
+
+
+
+        SqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            Console.WriteLine(dr[0].ToString() + " " + dr[1].ToString() + " " + dr[2].ToString() + " " + dr[3].ToString() + " " + dr[4].ToString() + " " + dr[5].ToString() + " " + dr[6].ToString() + " " + dr[7].ToString() + " " + dr[8].ToString() + " " + dr[9].ToString());
+            //Console.WriteLine(dr[1].ToString());
+            //Console.WriteLine(dr[2].ToString());
+            //Console.WriteLine(dr[3].ToString());
+
+        }
+        dr.Close();
+
+        sqlconn.Close();
+    }
+}
+
---
+++ 25aug/25augassign2/25augassign2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace assign2
+{
+    public class assign2
+    {
+        public static void Main(string[] args)
+        {
+            string connect = ConfigurationManager.ConnectionStrings["MyKey"].ConnectionString;
+            //string connect = "data source=LAPTOP-PH4JPVTP;initial catalog=demo;trusted_connection=true";
+
+            SqlConnection sqlconn = new SqlConnection(connect);
+            Console.WriteLine("connecting to the server");
+            sqlconn.Open();
+            Console.WriteLine("connected to the server");
+
+            Console.WriteLine("ENTER THE MOVIE GENRE");
+            string genre = Console.ReadLine();
+            SqlCommand cmd = new SqlCommand("select * from GetMovies(@genre)", sqlconn);
+
+            cmd.Parameters.Add("@genre", System.Data.SqlDbType.VarChar).Value = genre;
+            // cmd.ExecuteNonQuery();
+
+
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0].ToString() + " " + dr[1].ToString());
+                //Console.WriteLine(dr[1].ToString());
+                //Console.WriteLine(dr[2].ToString());
+                //Console.WriteLine(dr[3].ToString());
+            }
+            dr.Close();
+            sqlconn.Close();
+        }
+    }
+}
